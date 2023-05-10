ca9767382f2889e86cf55dc964dc1f54828fd00c
GuilhermeRatti <guirattimoraes@gmail.com>
2023-05-10 17:13:13 -0300

    multiplicacao por matrizes

--- Nodes.c
+++ Nodes.c
@@ -2,13 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Node
+{
+    float value;
+    int row;
+    int column;
+    struct Node *next_inrow;
+    struct Node *next_incolumn;
+};
+
 // Funcao que cria uma celula
 // Entrada: nenhum
 // Saida: ponteiro para a celula criada
 // Complexidade: O(1) (constante)
-Node *Node_cria(float value, int row, int column)
+pNode Node_cria(float value, int row, int column)
 {
-    Node *node = (Node *)calloc(1, sizeof(Node));
+    pNode node = (pNode)calloc(1, sizeof(struct Node));
     node->next_inrow = NULL;
     node->next_incolumn = NULL;
     node->value = value;
@@ -18,11 +27,71 @@ Node *Node_cria(float value, int row, int column)
     return node;
 }
 
+// Funcao que retorna a celula anterior
+// Entrada: ponteiro para a celula e direcao
+// Saida: ponteiro para a celula anterior
+// Complexidade: O(1) (constante)
+pNode Node_get_next(pNode node, Direction direction)
+{
+    if (direction == ROW)
+        return node->next_inrow;
+    else
+        return node->next_incolumn;
+}
+
+// Funcao que altera a celula anterior
+// Entrada: ponteiro para a celula, ponteiro para a nova celula e direcao
+// Saida: nenhum (muda por referencia)
+// Complexidade: O(1) (constante)
+void Node_set_next(pNode node, pNode next, Direction direction)
+{
+    if (direction == ROW)
+        node->next_inrow = next;
+    else
+        node->next_incolumn = next;
+}
+
+// Funcao que retorna a linha da celula
+// Entrada: ponteiro para a celula
+// Saida: linha da celula
+// Complexidade: O(1) (constante)
+float Node_get_value(pNode node)
+{
+    return node->value;
+}
+
+// Funcao que altera o valor da celula
+// Entrada: ponteiro para a celula e novo valor
+// Saida: nenhum (muda por referencia)
+// Complexidade: O(1) (constante)
+void Node_set_value(pNode node, float value)
+{
+    node->value = value;
+}
+
+// Funcao que retorna a linha da celula
+// Entrada: ponteiro para a celula
+// Saida: linha da celula
+// Complexidade: O(1) (constante)
+int Node_get_row(pNode node)
+{
+    return node->row;
+}
+
+// Funcao que retorna a coluna da celula
+// Entrada: ponteiro para a celula
+// Saida: coluna da celula
+// Complexidade: O(1) (constante)
+int Node_get_column(pNode node)
+{
+    return node->column;
+}
+
 // Funcao que destroi uma celula
 // Entrada: ponteiro para a celula
 // Saida: nenhum
 // Complexidade: O(1) (constante)
-void Node_destroi(Node *node)
+void Node_destroi(pNode node)
 {
     free(node);
     node = NULL;
--- Nodes.h
+++ Nodes.h
@@ -1,13 +1,12 @@
 #pragma once
 
-typedef struct Node
+typedef enum
 {
-    float value;
-    int row;
-    int column;
-    struct Node *next_inrow;
-    struct Node *next_incolumn;
-} Node;
+    ROW = 0,
+    COLUMN = 1
+} Direction;
+
+typedef struct Node *pNode;
 
 /**
  * @brief Constroi uma nova celula:: estrutura de celula
@@ -18,12 +17,82 @@ typedef struct Node
  * Linha da celula.
  * @param column
  * Coluna da celula.
- * @return Node*
+ * @return pNode
  * Ponteiro para a nova celula alocada.
  * @note
  * O usuario eh responsavel por liberar os ponteiros utilizando a funcao node_destroi().
  */
-Node *Node_cria(float value, int row, int column);
+pNode Node_cria(float value, int row, int column);
+
+/**
+ * @brief Retorna a celula anterior:: estrutura de celula
+ *  Retorna a celula anterior a celula dada.
+ * @param node
+ * Ponteiro para a celula a ser analisada.
+ * @param direction
+ * Direcao da celula a ser analisada.
+ * @return pNode
+ * Ponteiro para a celula posterior de acordo com a direcao.
+ */
+pNode Node_get_next(pNode node, Direction direction);
+
+/**
+ * @brief Altera a celula anterior:: estrutura de celula
+ *  Altera a celula anterior a celula dada.
+ * @param node
+ * Ponteiro para a celula a ser analisada.
+ * @param next
+ * Ponteiro para a nova celula posterior a celula dada.
+ * @param direction
+ * Direcao da celula a ser analisada.
+ * @return void
+ * @note
+ * Mudanca por referencia
+ */
+void Node_set_next(pNode node, pNode next, Direction direction);
+
+/**
+ * @brief Retorna o valor da celula:: estrutura de celula
+ *  Retorna o valor da celula dada.
+ * @param node
+ * Ponteiro para a celula a ser analisada.
+ * @return float
+ * Valor da celula.
+ */
+float Node_get_value(pNode node);
+
+/**
+ * @brief Altera o valor da celula:: estrutura de celula
+ *  Altera o valor da celula dada.
+ * @param node
+ * Ponteiro para a celula a ser analisada.
+ * @param value
+ * Novo valor da celula.
+ * @return void
+ * @note
+ * Mudanca por referencia
+ */
+void Node_set_value(pNode node, float value);
+
+/**
+ * @brief Retorna a coluna da celula:: estrutura de celula
+ *  Retorna a coluna da celula dada.
+ * @param node
+ * Ponteiro para a celula a ser analisada.
+ * @return int
+ * coluna da celula.
+ */
+int Node_get_column(pNode node);
+
+/**
+ * @brief Retorna a linha da celula:: estrutura de celula
+ *  Retorna a linha da celula dada.
+ * @param node
+ * Ponteiro para a celula a ser analisada.
+ * @return int
+ * linha da celula.
+ */
+int Node_get_row(pNode node);
 
 /**
  * @brief Destroi uma celula:: estrutura de celula
@@ -31,7 +100,5 @@ Node *Node_cria(float value, int row, int column);
  * @param node
  * Ponteiro para a celula a ser liberada.
  * @return void
- * @note
- * O ponteiro para a celula eh manualmente setado para NULL apos liberado por seguranca.
  */
-void Node_destroi(Node *node);
+void Node_destroi(pNode node);
